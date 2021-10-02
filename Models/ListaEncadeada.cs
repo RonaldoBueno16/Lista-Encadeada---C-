a169066aed5f50626db5a89cfd6aa3a48ed014af
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-10-02 11:27:08 -0300

    CRUD

--- Models/ListaEncadeada.cs
+++ Models/ListaEncadeada.cs
@@ -6,46 +6,17 @@ using System.Threading.Tasks;
 
 namespace Lista_Encadeada.Models
 {
-    class ListaEncadeada
+    class ListaEncadeadaModels
     {
         public Node primeiro { get; set; }
         public Node ultimo { get; set; }
         public int tamanho { get; set; }
 
-        public ListaEncadeada()
+        public ListaEncadeadaModels()
         {
             this.tamanho = 0;
             this.primeiro = null;
             this.ultimo = null;
-        }
-
-        public void InsertNode(Node node)
-        {
-            if (this.tamanho == 0)
-                this.primeiro = node;
-            else
-                this.ultimo.proximo = node;
-
-            this.tamanho++;
-            this.ultimo = node;
-        }
-
-        public override string ToString()
-        {
-            string retornar = "";
-
-            var no = this.primeiro;
-
-            while (true)
-            {
-                retornar += no.numero.ToString() + " ";
-
-                no = no.proximo;
-                if (no == null)
-                    break;
-            }
-
-            return retornar;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Lista_Encadeada.Models;
+﻿using Lista_Encadeada.Service;
+using Lista_Encadeada.Models;
 using System;
 
 namespace Lista_Encadeada
@@ -7,30 +8,19 @@ namespace Lista_Encadeada
     {
         static void Main(string[] args)
         {
-            Node teste1 = new Node(1);
-            Node teste2 = new Node(2);
-            Node teste3 = new Node(3);
-            Node teste4 = new Node(4);
-            Node teste5 = new Node(5);
-            Node teste6 = new Node(6);
-            Node teste7 = new Node(7); 
-            Node teste8 = new Node(8);
-            Node teste9 = new Node(9);
-            Node teste10 = new Node(12323);
-            Node teste11 = new Node(123123123);
+            ListaEncadeadaService lista = new ListaEncadeadaService();
+            for (int x = 0; x < 10; x++)
+            {
+                Node no = new Node(x+600);
+                lista.InsertNode(no);
+            }
 
-            ListaEncadeada lista = new ListaEncadeada();
-            lista.InsertNode(teste1);
-            lista.InsertNode(teste2);
-            lista.InsertNode(teste3);
-            lista.InsertNode(teste4);
-            lista.InsertNode(teste5);
-            lista.InsertNode(teste6);
-            lista.InsertNode(teste7);
-            lista.InsertNode(teste11);
-            lista.InsertNode(teste8);
-            lista.InsertNode(teste9);
-            lista.InsertNode(teste10);
+            Console.WriteLine(lista);
+            lista.SetValueForNode(5, 500);
+            Console.WriteLine(lista);
+            lista.RemoveNodeIndex(0);
+            lista.RemoveNodeIndex(0);
+            lista.RemoveNodeIndex(0);
             Console.WriteLine(lista);
         }
     }
